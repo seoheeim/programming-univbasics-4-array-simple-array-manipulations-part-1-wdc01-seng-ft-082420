2bfdc238521fea6aaedae692dd49d50b3c9a361a
Seohee  Im <seoheeim@vt.edu>
2020-07-21 18:45:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ end
 
 def pop_with_args(dog_breeds)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(2)
+  small_dogs = dog_breeds.pop(2)
 end
 
 def using_shift(my_favorite_city)
