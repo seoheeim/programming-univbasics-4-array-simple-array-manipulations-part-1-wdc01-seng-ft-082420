aad73a596dbedeef13f8e7f158fa41bf2a2fd89d
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:49:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,20 @@
-def using_push(Array, String) 
+def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  next_color = "violet"
-  updated_array = using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(bouroughs_in_nyc, 
+  
+end
+
+def using_pop
+end
+
+def pop_with_args
+end
+
+def using_shift
+end
+
+def shift_with_args 
+end
