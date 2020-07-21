1c86da4a88957a3c54261a07492819b24ef3720f
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:46:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,4 @@
+def using_push(Array, String) 
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  updated_array = using_push(colors_in_the_rainbow, next_color)
