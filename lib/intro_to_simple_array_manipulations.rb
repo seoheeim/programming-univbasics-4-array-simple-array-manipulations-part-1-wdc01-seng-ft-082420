50df76862c44e98b3961450d68028aca1329aae8
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:55:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,10 +10,13 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
 end
 
 def using_pop(continents, deleted_string)
-  
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  continents.pop
+  p continents
 end
 
-def pop_with_args
+def pop_with_args(dog_breeds, small_dogs)
+  dog_breeds = []
 end
 
 def using_shift
