db8d7fd57a160888d31cac55af62e23d54ccb40a
Seohee  Im <seoheeim@vt.edu>
2020-07-21 17:53:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,13 +4,13 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc = [""
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   bouroughs_in_nyc.unshift("Staten Island")
   p bouroughs_in_nyc
-  
 end
 
-def using_pop
+def using_pop(continents, deleted_string)
+  
 end
 
 def pop_with_args
